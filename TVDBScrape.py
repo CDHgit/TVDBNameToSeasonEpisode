0965263c1899ffb3044f32430c22a235a73658ee
CDHGit <collinhansen2@gmail.com>
2019-05-16 23:20:38 -0400

    Got it to parse season by season, moving on to getting out the episode names and adding them to a list from this somehow

--- TVDBScrape.py
+++ TVDBScrape.py
@@ -7,4 +7,9 @@ from bs4 import BeautifulSoup
 def scrape_from_url(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, features="html.parser")
-    print(soup)
+    boldHeadings = soup.h3
+    while boldHeadings:
+        print(boldHeadings)
+        entireSeason = boldHeadings.parent
+        print(entireSeason)
+        boldHeadings = boldHeadings.next_sibling
