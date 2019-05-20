cc9f1f77798a668187972573cd7f0c3129b25474
Collin Hansen <collinhansen2@gmail.com>
2019-05-20 09:24:07 -0400

    Changed return type to title and dict for folder renaming
    removed abundance of print statements

--- Main.py
+++ Main.py
@@ -4,8 +4,8 @@ from ScrapeToDict import dict_from_resp
 if __name__ == "__main__":
     urlToScrape = "https://www.thetvdb.com/series/spongebob-squarepants/seasons/all"
     response = request_from_url(urlToScrape)#  features="html.parser")
-    show_dict = dict_from_resp(response)
-    print(show_dict)
+    show_title, show_dict = dict_from_resp(response)
+    print(show_dict.items())
 
 
 
--- ScrapeToDict.py
+++ ScrapeToDict.py
@@ -5,6 +5,8 @@ from collections import defaultdict
 def dict_from_resp(response):
     season_episode_dict = defaultdict(list)
     soup = BeautifulSoup(response.text, features="html.parser")
+    show_title = remove_whitespaces(remove_tags(str(soup.h2.a)))
+    print(show_title)
     entire_season = soup.table
     season_number = header_to_season(str(soup.h3))
     while entire_season:
@@ -13,15 +15,15 @@ def dict_from_resp(response):
         while episode_in_season:
             episode_number_html = episode_in_season.td
             episode_name_html = episode_number_html.next_sibling.next_sibling
-            if season_number:
-                print("SEASON: " + season_number)
+            # if season_number:
+                # print("SEASON: " + season_number)
             if episode_number_html:
                 episode_number = remove_whitespaces(remove_tags(str(episode_number_html)))
-                print("NUMBER: " + episode_number)
+                # print("NUMBER: " + episode_number)
             if episode_name_html:
                 episode_name = remove_whitespaces(remove_tags(remove_whitespaces(str(episode_name_html.span))))
-                print("NAME: " + episode_name)
-            print()
+            #     print("NAME: " + episode_name)
+            # print()
             if season_number and episode_number and episode_name:
                 season_episode_dict[season_number].append((episode_number, episode_name))
             episode_in_season = episode_in_season.next_sibling.next_sibling
@@ -31,7 +33,8 @@ def dict_from_resp(response):
             entire_season = entire_season.next_sibling.next_sibling
         except AttributeError:
             break
-    return season_episode_dict
+    return show_title, season_episode_dict
+
 
 def header_to_season(header_tag):
     space_idx = header_tag.find(' ')
@@ -56,6 +59,3 @@ def remove_whitespaces(input_string):
     input_string = input_string.strip()
     input_string = input_string.replace("\n", "")
     return input_string
-
-
-
